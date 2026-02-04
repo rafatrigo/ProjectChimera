c897988e1e91471bcf46722d9447cdc5a344f92c
Cauã Anacleto <anacleto.for.work@gmail.com>
2026-02-04 16:33:02 -0300

    Refactor: Enhance interaction collision detection with ShapeCast3D with warning

--- scripts/player/interaction_manager.gd
+++ scripts/player/interaction_manager.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var interaction_ray = $RayCast3D
+@onready var shape_cast = $ShapeCast3D
 
 func _ready() -> void:
 	pass
@@ -13,7 +13,21 @@ func _process(_delta: float) -> void:
 	pass
 
 func check_interaction():
-	if interaction_ray.is_colliding():
-		var collider = interaction_ray.get_collider()
-		if collider.has_method("interact"):
-			collider.interact()
+	if shape_cast.is_colliding():
+
+		var closest_obj = null;
+		var shortest_dist = INF;
+
+		for i in shape_cast.get_collision_count():
+			var collider = shape_cast.get_collider(i);
+
+			var dist = global_position.distance_squared_to(collider.global_position)
+
+			if dist < shortest_dist:
+				shortest_dist = dist
+				closest_obj = collider
+
+		if closest_obj.has_method("interact"):
+			closest_obj.interact()
+		else:
+			push_warning("object marked as interactable has no method interact(). Obj name: " + closest_obj.name + " Warning from: " + self.name)

a10e131f6405181daa2fe7d38a3c952b10934f3b
Rafael <rafael.trigo.azevedo@gmail.com>
2026-02-04 16:46:35 -0300

    feat: player movement

--- scripts/player_3d.gd
+++ scripts/player_3d.gd
@@ -3,9 +3,14 @@ extends CharacterBody3D
 @export_group("Camera")
 @export_range(0.0, 1.0) var mouse_sensitivity := 0.25
 
+@export_group("Movement")
+@export var move_speed := 8.0
+@export var acceleration := 20.0
+
 var _camera_input_direction := Vector2.ZERO
 
 @onready var _camera_pivot: Node3D = %CameraPivot
+@onready var _camera: Camera3D = %Camera3D
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -23,6 +28,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		_camera_input_direction = event.screen_relative * mouse_sensitivity
 		
 func _physics_process(delta: float) -> void:
+	## CAMERA ##
 	# vetical rotation
 	_camera_pivot.rotation.x += _camera_input_direction.y * delta
 	
@@ -35,3 +41,16 @@ func _physics_process(delta: float) -> void:
 	# reset the moving direction to zero, preventing the camera to keep moving
 	_camera_input_direction = Vector2.ZERO
 	
+	## MOVEMENT ##
+	
+	var raw_input := Input.get_vector("move_left", "move_right", "move_front", "move_back")
+	var forward := _camera.global_basis.z
+	var right := _camera.global_basis.x
+	
+	var move_direction := forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0 # make sure that the character is moving parallel to the ground
+	move_direction = move_direction.normalized()
+	
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	
+	move_and_slide()
